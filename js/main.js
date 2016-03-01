85f0b12e2653589715265ef21405c734a047a2c0
PhiliPdB <phlpdbrn@gmail.com>
2016-03-01 20:31:15 +0100

    Improve header background calculation

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 window.onload = function() {
-	addEvent(window, 'scroll', (event) => {
-		let header = document.getElementById('header_background');
-		let banner = document.getElementById('home');
-		let scrollTop = window.scrollY;
-		let offset = {top: 0, left: 0};
-		let node = banner;
-		while (node) {
-			offset.top += node.offsetTop;
-			offset.left += node.offsetLeft;
-			node = node.offsetParent;
-		}
-		const height = banner.offsetHeight;
-		const range = 200;
+	addEvent(window, 'scroll', updateHeaderBackground);
+	updateHeaderBackground();
+}
 
-		offset = offset.top + height / 2;
-		let calc = (scrollTop - offset + range) / range;
+function updateHeaderBackground(event) {
+	let header = document.getElementById('header_background');
+	let banner = document.getElementById('home');
+	let scrollTop = window.scrollY;
+	let offset = {top: 0, left: 0};
+	let node = banner;
+	while (node) {
+		offset.top += node.offsetTop;
+		offset.left += node.offsetLeft;
+		node = node.offsetParent;
+	}
+	const height = banner.offsetHeight;
+	const range = 200;
 
-		header.style.opacity = calc;
-		if (calc > 1) {
-			header.style.opacity = 1;
-		} else if (calc < 0) {
-			header.style.opacity = 0;
-		}
-	});
+	offset = offset.top + height / 2;
+	let calc = (scrollTop - offset + range) / range;
+
+	header.style.opacity = calc;
+	if (calc > 1) {
+		header.style.opacity = 1;
+	} else if (calc < 0) {
+		header.style.opacity = 0;
+	}
 }
 
 // This is a function from https://github.com/remy/html5demos
