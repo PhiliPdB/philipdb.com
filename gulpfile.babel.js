89e8341d93d8427cba745d8a3fb20182ec9b4fa3
PhiliPdB <phlpdbrn@gmail.com>
2016-06-17 09:47:24 +0200

    Now deploying over SFTP

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -167,24 +167,19 @@ function deploy() {
 			user: $.util.env.user,
 			password: $.util.env.password,
 			remote_path: $.util.env.path
-		};
+		}
 	}
-
-	const connection = ftp.create({
-		host: config.host,
-		user: config.user,
-		password: config.password,
-		log: $.util.log
-	});
 
 	const globs = 'build/**';
 	const remotePath = $.util.env.beta ? config.beta_path : config.remote_path;
 
-	// using base = './build' will transfer everything to folder correctly 
-	// turn off buffering in gulp.src for best performance 
 	return gulp.src(globs, { base: './build', buffer: false })
-		.pipe(connection.newer(remotePath)) // only upload newer files 
-		.pipe(connection.dest(remotePath));
+		.pipe($.sftp({
+			host: config.host,
+			user: config.user,
+			pass: config.password,
+			remotePath: remotePath
+		}));
 }
 
 // MISC
