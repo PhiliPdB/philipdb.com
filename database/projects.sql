16782534d4b41676ec3c862201381f8eca6c1ebe
PhiliPdB <phlpdbrn@gmail.com>
2017-08-25 19:46:36 +0200

    Design database tables

---
+++ database/projects.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS website;
+USE website;
+
+DROP TABLE IF EXISTS projects;
+CREATE TABLE projects (
+	project_id INT(11) NOT NULL AUTO_INCREMENT,
+	project_title VARCHAR(255) NOT NULL,
+	project_subtitle VARCHAR(255) NOT NULL,
+	project_description TEXT NOT NULL,
+	project_date DATE NULL,
+	project_creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+	PRIMARY KEY (project_id)
+) ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS project_links;
+CREATE TABLE project_links (
+	project_link_id INT(11) NOT NULL AUTO_INCREMENT,
+	project_link_project_id INT(11) NOT NULL,
+	project_link_text VARCHAR(255) NOT NULL,
+	project_link_url VARCHAR(255) NOT NULL,
+
+	PRIMARY KEY (project_link_id)
+) ENGINE = InnoDB;
